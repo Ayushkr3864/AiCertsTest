ee02bb62838a8072005ddd7a32f32af708b2f399
Ayush Kumar Ray <ak05078189@gmail.com>
2025-12-14 14:53:28 +0530

    added

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,9 @@ const cors = require("cors")
 const app = express();
 app.use(
   cors({
-      origin: ["http://localhost:5173"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials:true
+    origin: ["http://localhost:5173", "https://ai-certs-test.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use(express.json());
